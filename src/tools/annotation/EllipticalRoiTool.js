1f91bf706372118d7d870d820093ac98d173f82f
Pooja-Kundaje <96064531+Pooja-Kundaje@users.noreply.github.com>
2023-01-10 15:18:20 -0500

    Enable rendering dashed lines dynamically for EllipticalRoi

--- src/tools/annotation/EllipticalRoiTool.js
+++ src/tools/annotation/EllipticalRoiTool.js
@@ -239,7 +239,7 @@ export default class EllipticalRoiTool extends BaseAnnotationTool {
 
         const ellipseOptions = { color };
 
-        if (renderDashed) {
+        if (renderDashed || data.dashed) {
           ellipseOptions.lineDash = lineDash;
         }
 
